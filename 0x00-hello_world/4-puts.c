bca4cb0acb542bc4b47862fce28ce2eee3be5bec
Amina El hakik <amina.el.hakik2@gmail.com>
2023-03-16 12:55:32 -0700

    r

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - prints "Programming is like building a multilingual puzzle"
+ * return 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
