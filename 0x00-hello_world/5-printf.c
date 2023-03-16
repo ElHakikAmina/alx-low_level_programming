d18fec4868b08583b1022c862d8d244acfef9a51
Amina El hakik <amina.el.hakik2@gmail.com>
2023-03-16 13:43:48 -0700

    r

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,13 @@
 
 
 /**
- * main Prints "with proper grammar, but the outcome
+ * main  Prints "with proper grammar, but the outcome
  * is a piece of art,"  followed by a new line.
  * Return : Always 0.
  */
 
 int main(void)
+
 
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
