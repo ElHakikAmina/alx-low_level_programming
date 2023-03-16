3d94a095b63867c2fb2b8744a079b24a132e41e0
Amina El hakik <amina.el.hakik2@gmail.com>
2023-03-16 15:47:24 -0700

    r

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include<unistd.h>
 
-/** main - prints "and that piece of art is useful" - Dora Kotpar, 2015-10-19",
- * followed by a newline, to stabdard error
+/** 
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a newline, to stabdard error.
  * Return: Always 1.
  */
 
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Koptar, 2015-10-19\n",59);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }
