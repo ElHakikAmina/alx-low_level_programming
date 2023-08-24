d6ed0211ef9d1d79f31f29f78c7f7a3502dcaffc
EL HAKIK Amina <amina@gmail.com>
2023-08-24 12:19:05 -0700

    fix error

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,8 +2,8 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
+ * list_len -function that  returns the number of elements in a linked list
+ * @h: pointer that point to  the list_t list
  *
  * Return: number of elements in h
  */
