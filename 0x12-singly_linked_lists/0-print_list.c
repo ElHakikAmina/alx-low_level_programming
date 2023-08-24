8dd42ab2cd4327749ece44ba24d1a9b9c631fd12
EL HAKIK Amina <amina@gmail.com>
2023-08-24 11:59:32 -0700

    modification

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,7 +3,6 @@
 /**
  * print_list - prints all the elements of a linked list
  * @h: pointer to the list_t list to print
- *
  * Return: the number of nodes printed
  */
 size_t print_list(const list_t *h)
